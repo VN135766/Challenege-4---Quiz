9e61b92d814b6bf1c1000ec78d046e2ef623f752
Victor Nunez <VN135766@gmail.com>
2022-04-28 14:12:45 -0400

    add questions

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        question: "Commonly used data types do not include?",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts",
+    },
+    {
+        question: "The condition in an IF / ELSE statement is enclosed within ____?",
+        choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Parentheses",
+    },
+    {
+        question: "Arrays in JaveScript can be used to store?",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above",
+    },
+    {
+        question: "String variables must be enclosed within ___, when being assigned to variables",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Curly Brackets",
+    },
+    {
+        question: "A very useful took used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+        answer: "Console.log",
+    },
