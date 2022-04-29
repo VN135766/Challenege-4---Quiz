af0dd8bff7ba706c0519d7af017e4f6a408592d0
Victor Nunez <VN135766@gmail.com>
2022-04-29 00:35:35 -0400

    add js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+var startTime = 60;
 var questions = [
     {
         question: "Commonly used data types do not include?",
@@ -23,4 +24,20 @@ var questions = [
         question: "A very useful took used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
         answer: "Console.log",
-    },
+    },];
+
+
+var startQuiz = function () {
+    var x = setInterval(function () {
+        startTime--;
+        document.querySelector("#timer").textContent = "Timer " + startTime;
+    }, 1000);
+    document.querySelector("#question").textContent = questions[0].question;
+    var taskLi = $("<li>")
+    for (let i = 0; i < questions[0].choices.length; i++) {
+        text += "<li>" + questions[0].choices[i] + "</li>"
+        querySelector("#list-question").appendChild(text);
+    }
+
+}
+
